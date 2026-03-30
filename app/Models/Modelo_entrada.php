763a67d416d0a113743ae44c676a480503f2f7b4
arcos2999-cpu <arcos2999@gmail.com>
2026-03-30 01:20:42 -0600

    Cambios en modelo entrada

--- app/Models/Modelo_entrada.php
+++ app/Models/Modelo_entrada.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class Modelo_entrada extends Model{
-    protected $table      = 'entradas';
+    protected $table      = 'entrada';
     // Uncomment below if you want add primary key
     protected $primaryKey = 'id';
     protected $allowedFields = ['fecha','fecha_cad','cantidad','u_compra','u_venta','precio_compra','id_producto']; 
